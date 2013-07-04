a2f6bb63fc58b6058a97347d184efa007fedc70a
Matt Lee <thatismatt@gmail.com>
2013-07-04 01:12:40 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,163 @@
+(function (cromp) {
+    function id (x) { return x; }
+    function toArray (a) { return Array.prototype.slice.call(a, 0); }
+    function flatten (xs) {
+        return xs.reduce(function (a, x) {
+            Array.prototype.push.apply(a, x);
+            return a;
+        }, []);
+    }
+
+    function Parser (parse) {
+        this.parse = parse;
+    }
+
+    Parser.prototype.map = function (f) {
+        var self = this;
+        return new Parser(function (state) {
+            var x = self.parse(state);
+            return x.success ? success(f(x.result), x.state) : x;
+        });
+    };
+
+    function ParseState (source, index) {
+        this.source = source;
+        this.index = index || 0;
+    }
+
+    ParseState.prototype.current = function () {
+        return this.source[this.index];
+    };
+
+    ParseState.prototype.forward = function (x) {
+        return new ParseState(this.source, this.index + (x || 1));
+    };
+
+    ParseState.prototype.back = function () {
+        return new ParseState(this.source, this.index - 1);
+    };
+
+    function success (result, state) {
+        return {
+            state: state,
+            success: true,
+            result: result
+        };
+    }
+
+    function fail (state) {
+        return {
+            state: state,
+            success: false
+        };
+    }
+
+    cromp.parse = function (parser, source) {
+        return parser.parse(new ParseState(source));
+    };
+
+    cromp.seq = function () {
+        var parsers = toArray(arguments);
+
+        function parse (state) {
+            return parsers.reduce(
+                function (agg, p) {
+                    if (!agg.success) return agg;
+                    if (p.call) p = p();
+                    var x = p.parse(agg.state);
+                    return x.success
+                        ? (agg.result.push(x.result),
+                           success(agg.result, x.state))
+                        : fail(state);
+                }, success([], state));
+        }
+
+        return new Parser(parse);
+    };
+
+    cromp.choose = function () {
+        var parsers = toArray(arguments);
+
+        function parse (state) {
+            return parsers.reduce(
+                function (agg, p) {
+                    if (agg.success) return agg;
+                    var x = p.parse(state);
+                    return x.success ? x : agg;
+                }, fail(state));
+        }
+
+        return new Parser(parse);
+    };
+
+    cromp.optional = function (parser) {
+        return new Parser(function (state) {
+            //if (parser.call) parser = parser();
+            var x = parser.parse(state);
+            return success(x.result, x.state);
+        });
+    };
+
+    cromp.many = function (parser, min) {
+        return new Parser(function (state) {
+            if (parser.call) parser = parser();
+            var result = [];
+            var x, s = state;
+            function p () { x = parser.parse(s); s = x.state; }
+            for (p(); x.success; p()) {
+                result.push(x.result);
+            }
+            return result.length >= (min || 0) ? success(result, s) : fail(state);
+        });
+    };
+
+    cromp.interpose = function (pa, pb) {
+        return new Parser(function (state) {
+            var a = pa.parse(state);
+            var pba = cromp.many(cromp.seq(pb, pa));
+            if (a.success) {
+                var x = pba.parse(a.state);
+                var result = flatten(x.result);
+                result.unshift(a.result);
+                return success(result, x.state);
+            } else {
+                return fail(state);
+            }
+        });
+    };
+
+    cromp.chainl = function (p, op) {
+        return new Parser(function (state) {
+            var l = p.parse(state);
+            function rest (m) {
+                var n = cromp.seq(op, p).parse(m.state);
+                return n.success
+                    ? rest(success(n.result[0](m.result, n.result[1]), n.state))
+                    : success(m.result, m.state);
+            }
+            return l.success ? rest(l) : fail(state);
+        });
+    };
+
+    cromp.character = function (ch, f) {
+        f = f || id;
+        return new Parser(function (state) {
+            return state.current() === ch
+                ? success(f(ch), state.forward())
+                : fail(state);
+        });
+    };
+
+    cromp.regex = function (re) {
+        return new Parser(function (state) {
+            var rest = state.source.substring(state.index);
+            var match = re.exec(rest);
+            if (match && match.index === 0) {
+                return success(match, state.forward(match[0].length));
+            } else {
+                return fail(state);
+            }
+        });
+    };
+
+})(typeof exports === "undefined" ? this["cromp"] = {} : exports);
---
+++ test/cromp.tests.js
@@ -0,0 +1,239 @@
+var cromp = require("../");
+
+exports["parse character a"] = function(test) {
+    var parser = cromp.character("a", function(ch) {
+        return "got " + ch;
+    });
+    var result = cromp.parse(parser, "a").result;
+    test.equal(result, "got a");
+    test.done();
+};
+
+exports["parse character b"] = function(test) {
+    var parser = cromp.character("b", function(ch) {
+        return "got " + ch;
+    });
+    var result = cromp.parse(parser, "b").result;
+    test.equal(result, "got b");
+    test.done();
+};
+
+exports["parse characters ab"] = function(test) {
+    var parser = cromp.seq(
+        cromp.character("a"),
+        cromp.character("b"))
+    .map(function (results) {
+        return "got " + results[0] + " then " + results[1];
+    });
+    var result = cromp.parse(parser, "ab").result;
+    test.equal(result, "got a then b");
+    test.done();
+};
+
+exports["parse characters ba"] = function(test) {
+    var parser = cromp.seq(
+        cromp.character("b"),
+        cromp.character("a"))
+    .map(function (results) {
+        return "got " + results[0] + " then " + results[1];
+    });
+    var result = cromp.parse(parser, "ba").result;
+    test.equal(result, "got b then a");
+    test.done();
+};
+
+exports["can't parse a as character b"] = function(test) {
+    var parser = cromp.character("b");
+    var x = cromp.parse(parser, "a");
+    test.ok(!x.success);
+    test.done();
+};
+
+exports["can't parse aa as characters a then b"] = function(test) {
+    var parser = cromp.seq(
+        cromp.character("a"),
+        cromp.character("b"));
+    var x = cromp.parse(parser, "aa");
+    test.ok(!x.success);
+    test.done();
+};
+
+exports["simple choice parsing"] = function(test) {
+    var parser = cromp.choose(
+        cromp.character("a"),
+        cromp.character("b"));
+    var a = cromp.parse(parser, "a");
+    var b = cromp.parse(parser, "b");
+    test.ok(a.success);
+    test.ok(b.success);
+    test.done();
+};
+
+exports["choice parsing"] = function(test) {
+    var parser = cromp.choose(
+        cromp.seq(
+            cromp.character("a"),
+            cromp.character("b")),
+        cromp.seq(
+            cromp.character("a"),
+            cromp.character("a")));
+    var ab = cromp.parse(parser, "ab");
+    var aa = cromp.parse(parser, "aa");
+    test.ok(ab.success);
+    test.ok(aa.success);
+    test.done();
+};
+
+exports["optional parsing"] = function (test) {
+    var parser = cromp.seq(
+            cromp.optional(cromp.character("a")),
+            cromp.character("b"));
+    var ab = cromp.parse(parser, "ab");
+    var b = cromp.parse(parser, "b");
+    var aa = cromp.parse(parser, "aa");
+    test.ok(ab.success);
+    test.ok(b.success);
+    test.ok(!aa.success);
+    test.done();
+};
+
+exports["many parsing"] = function (test) {
+    var parser = cromp.many(
+            cromp.character("a"));
+    var a = cromp.parse(parser, "a");
+    var aa = cromp.parse(parser, "aa");
+    var aaa = cromp.parse(parser, "aaa");
+    var b = cromp.parse(parser, "b");
+    test.ok(a.success);
+    test.deepEqual(["a"], a.result);
+    test.ok(aa.success);
+    test.deepEqual(["a", "a"], aa.result);
+    test.ok(aaa.success);
+    test.deepEqual(["a", "a", "a"], aaa.result);
+    test.ok(b.success);
+    test.deepEqual([], b.result);
+    test.done();
+};
+
+exports["interpose parsing"] = function (test) {
+    var parser = cromp.interpose(
+            cromp.character("a"),
+            cromp.character(","));
+    var a = cromp.parse(parser, "a");
+    var aa = cromp.parse(parser, "a,a");
+    var aaa = cromp.parse(parser, "a,a,a");
+    var b = cromp.parse(parser, "b");
+    test.ok(a.success);
+    test.deepEqual(["a"], a.result);
+    test.ok(aa.success);
+    test.deepEqual(["a", ",", "a"], aa.result);
+    test.ok(aaa.success);
+    test.deepEqual(["a", ",", "a", ",", "a"], aaa.result);
+    test.ok(!b.success);
+    test.done();
+};
+
+exports["parse regex"] = function (test) {
+    var parser = cromp.regex(/[a-c]/);
+    var a = cromp.parse(parser, "a");
+    var x = cromp.parse(parser, "x");
+    var xa = cromp.parse(parser, "xa");
+    test.ok(a.success);
+    test.deepEqual("a", a.result[0]);
+    test.ok(!x.success);
+    test.ok(!xa.success);
+    test.done();
+};
+
+exports["parse simple calculator grammar"] = function (test) {
+    var num = cromp.regex(/[0-9]+/).map(function (m) { return parseInt(m[0], 10); });
+    var op1 = cromp.regex(/[+-]/).map(function (m) { return m[0]; });
+    var op2 = cromp.regex(/[*\/]/).map(function (m) { return m[0]; });
+    function getTerm () { return term; } // late binding
+    var term = cromp.choose(
+        cromp.seq(num, op2, getTerm)
+            .map(function (x) { return { op: x[1], l: x[0], r: x[2] }; }),
+        num);
+    function getExpr () { return expr; } // late binding
+    var expr = cromp.choose(
+        cromp.seq(term, op1, getExpr)
+            .map(function (x) { return { op: x[1], l: x[0], r: x[2] }; }),
+        term);
+    var parse = function (src) { return cromp.parse(expr, src); };
+
+    var a = parse("1");
+    var b = parse("1+2");
+    var c = parse("1*2");
+    var d = parse("1+2*3+4");
+    test.ok(a.success);
+    test.ok(b.success);
+    test.ok(c.success);
+    test.ok(d.success);
+    test.done();
+};
+
+exports["parse left associative grammar"] = function (test) {
+    var num = cromp.regex(/[0-9]+/).map(function (m) { return parseInt(m[0], 10); });
+    var op = cromp.character("+").map(function (x) {
+        return function(l, r) { return ["+", l, r]; }; });
+    var expr = cromp.chainl(num, op);
+    var parse = function (src) { return cromp.parse(expr, src); };
+
+    var a = parse("1");
+    var b = parse("1+2");
+    var c = parse("1+2+3");
+    var d = parse("1+2+3+4");
+    test.ok(a.success);
+    test.ok(b.success);
+    test.ok(c.success);
+    test.deepEqual(c.result, ["+", ["+", 1, 2], 3]);
+    test.ok(d.success);
+    test.deepEqual(d.result, ["+", ["+", ["+", 1, 2], 3], 4]);
+    test.done();
+};
+
+exports["parse nested lists"] = function (test) {
+    var open = cromp.character("(");
+    var close = cromp.character(")");
+    function list_ () { return list; } // late binding
+    var list = cromp.seq(open, cromp.many(list_), close)
+            .map(function (x) { return x[1]; });
+    var parse = function (src) { return cromp.parse(list, src); };
+
+    var a = parse("()");
+    var b = parse("((()))");
+    var c = parse("(()())");
+    var d = parse("(()(())())");
+    var e = parse("(((())");
+    test.ok(a.success);
+    test.deepEqual(a.result, []);
+    test.ok(b.success);
+    test.deepEqual(b.result, [[[]]]);
+    test.ok(c.success);
+    test.deepEqual(c.result, [[],[]]);
+    test.ok(d.success);
+    test.deepEqual(d.result, [[],[[]],[]]);
+    test.ok(!e.success);
+    test.done();
+};
+
+// COMBINATORS
+// cromp.eof
+// cromp.not or notFollowedBy
+// cromp.many(min) or many1
+// cromp.manyTill
+// cromp.whitespace
+// cromp.string
+// cromp.between(open, close, p)
+
+// ERRORS
+// incomplete parse
+// parse failures
+// badly constructed parser
+
+// Parser.mapcat
+// Parser.filter
+
+// TEST CASES
+// if c then t else f
+// JSON
